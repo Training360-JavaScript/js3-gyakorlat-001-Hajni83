666b91def4d2af8b84ea2ad2d0c03aa89d5cc63d
Hajni83 <maro.hajnalka@gmail.com>
2021-11-09 23:03:23 +0100

    2 task

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,15 @@
+const HU = {
+  date(now) {
+    return new Intl.DateTimeFormat("hu-HU").format(now);
+  },
+  curreny(currency) {
+    const currencyFormat = new Intl.NumberFormat("hu-HU", {
+      style: "currency",
+      currency: "HUF",
+    });
+    return currencyFormat.format(currency);
+  },
+  list() {},
+};
 
-
-
-
-
-
-// export default HU
+export default HU;
